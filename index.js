ecd672ac39a8bbe864dc1f1151d49f78ca4cb3bd
Hifi58 <gillet.tony@gmail.com>
2023-10-08 17:08:47 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,31 @@
+import { TOKEN } from "./auth.js";
+
+const { Client, GatewayIntentBits } = require("discord.js");
+const PREFIX = "!!";
+const client = new Client({
+    intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,],
+});
+
+client.on("ready", () => {
+    client.user.setPresence({
+        activities: [{
+            name: "répondre aux questions"
+        }],
+        status: "online"
+    });
+
+    console.log(`L'Éternelle est arrivé pour répondre à vos questions !`)
+});
+
+client.on("messageCreate", async message => {
+    if(message.content.startsWith(PREFIX)){
+        message.reply("qui ose me sortir de mon sommeil ?")
+    }
+});
+
+
+client.login(TOKEN);
